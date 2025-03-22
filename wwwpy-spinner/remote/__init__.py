da18145bece790014e747cb099cb64c8d2ede885
simone <simone.giacomelli@gmail.com>
2025-03-22 09:02:17 +0000

    feat: add HTML styles and meta tags to enhance document presentation

--- wwwpy-spinner/remote/__init__.py
+++ wwwpy-spinner/remote/__init__.py
@@ -2,8 +2,26 @@ from js import document
 
 
 async def main():
-    from wwwpy.remote import shoelace
-    shoelace.setup_shoelace()
-
+    document.head.insertAdjacentHTML('beforeend', _style)
     from . import component1  # for component registration
     document.body.innerHTML = '<component-1></component-1>'
+
+
+
+# language=html
+_style = """
+<link rel="icon" href="data:,">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<style>
+    body {
+        background: #121212;
+        color: #e0e0e0;
+        margin: 1rem;
+        font: 16px sans-serif;
+    }
+
+    a {
+        color: #bb86fc
+    }
+</style>
+"""
