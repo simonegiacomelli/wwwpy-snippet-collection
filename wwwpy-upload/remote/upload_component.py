89bb19b06cc86daa34f4a8d1bcfc9d1d07782c2e
simone <simone.giacomelli@gmail.com>
2025-03-22 13:11:42 +0000

    feat: refactor upload component to enhance progress tracking and UI updates

--- wwwpy-upload/remote/upload_component.py
+++ wwwpy-upload/remote/upload_component.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import asyncio
 import base64
 import logging
@@ -72,12 +73,56 @@ class UploadComponent(wpc.Component, tag_name='wwwpy-quickstart-upload'):
         for file in files:
             progress = UploadProgressComponent()
             self.uploads.appendChild(progress.element)
-            asyncio.create_task(progress.upload(file))
+            asyncio.create_task(upload_file(file, progress.update_progress))
 
     async def file_input__change(self, event):
         files = self.file_input.files
         self._process_files(files)
         self.file_input.value = ''
+
+
+class UploadProgressComponent(wpc.Component):
+    file_input: js.HTMLInputElement = wpc.element()
+    progress: js.HTMLProgressElement = wpc.element()
+    progress_label: js.HTMLLabelElement = wpc.element()
+    label: js.HTMLInputElement = wpc.element()
+
+    def init_component(self):
+        # language=html
+        self.element.innerHTML = """
+<div data-name="progress_label" style='padding: 0.5em'>
+    <progress data-name="progress"></progress>
+    &nbsp;<span data-name="label"></span>
+</div>"""
+
+    def update_progress(self, progress: UploadProgress):
+        """Callback function to update the UI with upload progress."""
+        self.progress.max = progress.total_bytes
+        self.progress.value = progress.bytes_uploaded
+
+        # Update label
+        label = f'{progress.file.name}: '
+        if progress.completed:
+            label += 'upload completed'
+            self._fade_out()
+        elif progress.failure:
+            label += f'error: {progress.failure}'
+        else:
+            label += f'{progress.percentage}%'
+
+        self.label.textContent = label
+
+    def _fade_out(self, fade_delay_secs=3):
+        """Fade out and remove the progress element after completion."""
+        self.element.style.opacity = '1'
+        self.element.style.transition = f'opacity {fade_delay_secs}s'
+        self.element.style.opacity = '0'
+
+        async def _remove():
+            await asyncio.sleep(fade_delay_secs)
+            self.element.remove()
+
+        asyncio.create_task(_remove())
 
 
 @dataclass
@@ -133,9 +178,11 @@ async def _read_chunk(blob: js.Blob) -> js.ArrayBuffer:
     return await future
 
 
-async def upload_file(file: js.File,
-                      progress_callback: Callable[[UploadProgress], None] = None,
-                      chunk_size: int = pow(2, 18)) -> None:
+async def upload_file(
+        file: js.File,
+        progress_callback: Callable[[UploadProgress], None] = None,
+        chunk_size: int = pow(2, 18)
+) -> None:
     """
     Upload a file in chunks to the server.
 
@@ -188,53 +235,3 @@ async def upload_file(file: js.File,
         logger.exception(e)
         progress.failure = e
         progress_callback(progress)
-
-
-
-class UploadProgressComponent(wpc.Component):
-    file_input: js.HTMLInputElement = wpc.element()
-    progress: js.HTMLProgressElement = wpc.element()
-    progress_label: js.HTMLLabelElement = wpc.element()
-    label: js.HTMLInputElement = wpc.element()
-
-    def init_component(self):
-        # language=html
-        self.element.innerHTML = """
-<div data-name="progress_label" style='padding: 0.5em'>
-    <progress data-name="progress"></progress>
-    &nbsp;<span data-name="label"></span>
-</div>"""
-
-    async def upload(self, file: js.File, chunk_size: int = pow(2, 18)):
-        """Upload a file and update the UI with progress."""
-        await upload_file(file, self._update_progress, chunk_size)
-
-
-    def _update_progress(self, progress: UploadProgress):
-        """Callback function to update the UI with upload progress."""
-        self.progress.max = progress.total_bytes
-        self.progress.value = progress.bytes_uploaded
-
-        # Update label
-        label = f'{progress.file.name}: '
-        if progress.completed:
-            label += 'upload completed'
-            self._fade_out()
-        elif progress.failure:
-            label += f'error: {progress.failure}'
-        else:
-            label += f'{progress.percentage}%'
-
-        self.label.textContent = label
-
-    def _fade_out(self, fade_delay_secs=3):
-        """Fade out and remove the progress element after completion."""
-        self.element.style.opacity = '1'
-        self.element.style.transition = f'opacity {fade_delay_secs}s'
-        self.element.style.opacity = '0'
-
-        async def _remove():
-            await asyncio.sleep(fade_delay_secs)
-            self.element.remove()
-
-        asyncio.create_task(_remove())
