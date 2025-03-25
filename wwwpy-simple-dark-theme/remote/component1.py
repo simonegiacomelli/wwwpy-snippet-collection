a5ba56a15a5302d40a5486a73b0b4f5ebe0c23b0
simone <simone.giacomelli@gmail.com>
2025-03-25 10:32:15 +0000

    feat: implement simple dark theme by adding theme header and refactoring main component

--- wwwpy-simple-dark-theme/remote/component1.py
+++ wwwpy-simple-dark-theme/remote/component1.py
@@ -1,3 +1,4 @@
+import inspect
 import wwwpy.remote.component as wpc
 import js
 
@@ -6,7 +7,9 @@ import logging
 logger = logging.getLogger(__name__)
 
 class Component1(wpc.Component, tag_name='component-1'):
+    button1: js.HTMLButtonElement = wpc.element()
     def init_component(self):
         # language=html
         self.element.innerHTML = """
-<div>component-1</div>"""
+<div>component-1</div>
+<button data-name="button1">button1</button>"""
