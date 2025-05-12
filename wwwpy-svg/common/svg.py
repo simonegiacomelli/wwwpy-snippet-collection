7c2f7c775497af94dc95d94cdd34b15ae04f5723
simone <simone.giacomelli@gmail.com>
2025-05-12 12:25:35 +0100

    feat: refactor add_rounded_background2 and add_rounded_background3 functions for improved readability and parameter consistency

--- wwwpy-svg/common/svg.py
+++ wwwpy-svg/common/svg.py
@@ -3,21 +3,24 @@ from typing import Callable, Dict
 
 import xml.etree.ElementTree as ET
 
+
 def set_width_height(svg: str, width: float, height: float) -> str:
     root = ET.fromstring(svg)
     root.set('width', str(width))
     root.set('height', str(height))
+    # return ET.tostring(root, encoding='unicode')
     out = ET.tostring(root, encoding='unicode')
     out = out.replace('ns0:', '').replace('xmlns:ns0=', 'xmlns=')
     return out
 
 
-def add_rounded_background2(svg: str, fill_color: str, ratio: float =1.4, radius: float = 5) -> str:
+def add_rounded_background2(svg: str, fill_color: str, ratio: float = 1.4, radius: float = 5) -> str:
     r = add_rounded_background3(svg, fill_color, ratio, radius)
     r2 = set_width_height(r, 22, 22)
     return r2
 
-def add_rounded_background3(svg: str, fill_color: str, ratio: float =1.4, radius: float = 5) -> str:
+
+def add_rounded_background3(svg: str, fill_color: str, ratio: float = 1.4, radius: float = 5) -> str:
     root = ET.fromstring(svg)
     orig_w = float(root.attrib['width'])
     orig_h = float(root.attrib['height'])
@@ -30,8 +33,10 @@ def add_rounded_background3(svg: str, fill_color: str, ratio: float =1.4, radius
     assert vb_w == vb_h == orig_w
     new_size = orig_w * ratio
     offset = (orig_w - new_size) / 2
+
     def fmt(v):
         return str(int(v)) if v.is_integer() else str(v)
+
     new_s = fmt(new_size)
     off_s = fmt(offset)
     root.attrib['viewBox'] = f'{off_s} {off_s} {new_s} {new_s}'
