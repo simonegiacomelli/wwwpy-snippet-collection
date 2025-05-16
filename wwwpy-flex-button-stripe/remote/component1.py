1dea1063d1524744330cbdade3ff8fda154a662f
simone <simone.giacomelli@gmail.com>
2025-05-16 16:47:36 +0100

    feat: enhance Component1 with SVG integration and logging functionality

--- wwwpy-flex-button-stripe/remote/component1.py
+++ wwwpy-flex-button-stripe/remote/component1.py
@@ -1,15 +1,53 @@
+import inspect
+from pathlib import Path
+
 import wwwpy.remote.component as wpc
 import js
 
 import logging
 
+from wwwpy.common.designer.ui.svg import add_rounded_background2
+import wwwpy.remote.designer.ui.new_toolbox  # noqa
+from wwwpy.remote import dict_to_js
+
 logger = logging.getLogger(__name__)
+
+_BLUE = '#3574F0'
+_GRAY = '#3C3E41'
+_BGRD = '#2B2D30'
 
 
 class Component1(wpc.Component, tag_name='component-1'):
+    _talogs: js.HTMLTextAreaElement = wpc.element()
+    div1: js.HTMLDivElement = wpc.element()
 
     def init_component(self):
         # language=html
         self.element.innerHTML = """
-<div>component-1</div>
+        <wwwpy-new-toolbox></wwwpy-new-toolbox>
+        <div>component-1</div>
+<textarea data-name="_talogs" placeholder="textarea1" rows="6" wrap="off" style="width: 100%"></textarea>
+<div data-name="div1" style="display: flex; gap: 5px;  background-color: #2B2D30">
+</div>
+
 """
+        self._add_svg()
+
+    def _add_svg(self):
+        folder = Path(__file__).parent
+        # list all svgs
+        svgs = list(folder.glob('*.svg'))
+        for svg in svgs:
+            svg_str = svg.read_text()
+            svg_str = add_rounded_background2(svg_str, _BGRD)
+            r = js.document.createRange().createContextualFragment(svg_str)
+            self.div1.appendChild(r)
+            self._log(str(svg))
+
+    def _log(self, msg):
+        self._talogs.value += str(msg) + '\n'
+        # scroll textarea to bottom
+        self._talogs.scrollTop = self._talogs.scrollHeight
+
+
+        # svg_str = add_rounded_background2(_svg1, color)
