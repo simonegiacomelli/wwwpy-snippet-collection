7cedb3b7803e6021cd7c13e71a9907557ec4e147
simone <simone.giacomelli@gmail.com>
2025-05-30 19:31:35 +0100

    feat: rename Component1 to LoggerConfigComponent and enhance logger configuration functionality

--- wwwpy-logging-playground/remote/__init__.py
+++ wwwpy-logging-playground/remote/__init__.py
@@ -5,5 +5,5 @@ async def main():
     from wwwpy.remote import shoelace
     shoelace.setup_shoelace()
 
-    from . import component1  # for component registration
+    from . import logger_config  # for component registration
     document.body.innerHTML = '<component-1></component-1>'
