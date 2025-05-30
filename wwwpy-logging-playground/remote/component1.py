421a9b48fd0be17f3e4646eb66c0384be7fdb017
simone <simone.giacomelli@gmail.com>
2025-05-30 19:12:49 +0100

    feat: implement search functionality in Component1 and refactor LogConfRow properties

--- wwwpy-logging-playground/remote/component1.py
+++ wwwpy-logging-playground/remote/component1.py
@@ -6,15 +6,15 @@ import logging
 import wwwpy.remote.component as wpc
 
 logger = logging.getLogger(__name__)
-from . import log_conf_row # noqa
+from . import log_conf_row  # noqa
+
 
 class Component1(wpc.Component, tag_name='component-1'):
     _rescan: js.HTMLButtonElement = wpc.element()
     _ta_log: js.HTMLTextAreaElement = wpc.element()
     _title: js.HTMLDivElement = wpc.element()
-    logConfRow2: LogConfRow = wpc.element()
     _row_container: js.HTMLDivElement = wpc.element()
-    input1: js.HTMLInputElement = wpc.element()
+    _search: js.HTMLInputElement = wpc.element()
 
     def init_component(self):
         # language=html
@@ -24,9 +24,11 @@ class Component1(wpc.Component, tag_name='component-1'):
 
 <div data-name="_title">_title</div><textarea data-name="_ta_log" placeholder="textarea1" rows="8" wrap="off" style="width: 100%"></textarea>
 
-<log-conf-row data-name="logConfRow1"></log-conf-row><log-conf-row data-name="logConfRow2"></log-conf-row>
 
-<input data-name="input1" placeholder="input1" type="checkbox"><div data-name="_row_container"><br></div> 
+
+
+<input data-name="_search" placeholder="Filter by logger name">
+<div data-name="_row_container"><br></div> 
 """
         self._list_all_logger()
 
@@ -40,12 +42,15 @@ class Component1(wpc.Component, tag_name='component-1'):
         self._title.innerText = f'Found {len(items)} loggers'
         self._row_container.innerHTML = ''  # clear previous rows
         for name, logger in items:
+            search_value = self._search.value.lower()
+            if search_value and search_value not in name.lower():
+                continue
             level_name = '' if logger.level == logging.NOTSET else logging.getLevelName(logger.level)
             line = f"{level_name:<10} {name}"
             self._log(line)
             row = log_conf_row.LogConfRow()
-            row.logger_name.innerText = name
-            row.level_select.value = level_name
+            row.logger_name = name
+            row.level_select = level_name
             self._row_container.appendChild(row.element)
         # scroll to the top of the log area
         self._ta_log.scrollTop = 0
@@ -58,7 +63,17 @@ class Component1(wpc.Component, tag_name='component-1'):
 
     async def _rescan__click(self, event):
         self._list_all_logger()
-    
-    async def _row_container__input(self, event):
+
+    async def _row_container__input_row(self, event):
         logger.debug(f'{inspect.currentframe().f_code.co_name} event fired %s', event)
+        row: LogConfRow = event.detail
+        logger.debug(f'LogConfRow: _row_container__input_row: {row.logger_name} level={row.level_select}')
+        logging.getLogger(row.logger_name).setLevel(row.level_select)
+        
     
+    async def _search__input(self, event):
+        logger.debug(f'{inspect.currentframe().f_code.co_name} event fired %s', event)
+        self._list_all_logger()
+    
+    
+
