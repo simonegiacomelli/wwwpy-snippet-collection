0f246382d011c13e1c9aa44dea1673eb8952b3dd
simone <simone.giacomelli@gmail.com>
2025-07-11 11:11:12 +0100

    Refactor icon handling to use AllIcons for clarity.
    Replaced file-based SVG loading with AllIcons to streamline the process and improve code readability. Added explicit logging for better tracing of which icons are being added.

--- wwwpy-flex-button-stripe/remote/component1.py
+++ wwwpy-flex-button-stripe/remote/component1.py
@@ -5,6 +5,7 @@ from pathlib import Path
 
 import js
 import wwwpy.remote.component as wpc
+from wwwpy.common.designer.ui.icons.all_icons import AllIcons
 from wwwpy.remote.designer.ui.svg_icon import SvgIcon
 
 logger = logging.getLogger(__name__)
@@ -23,9 +24,11 @@ class Component1(wpc.Component, tag_name='component-1'):
 <div data-name="_vertical" style="display: inline-flex; flex-direction: column;  background-color: #2B2D30"></div>
 <hr>
 """
-        folder = Path(__file__).parent
-
-        svgs = list(folder.glob('*.svg'))
-        for svg in svgs:
+        logger.debug('Adding icons')
+        for svg in AllIcons.all_icons():
+            logger.debug(f'Adding icon: {svg}')
             self._horizontal.appendChild(SvgIcon.from_file(svg).element)
+
+        for svg in [AllIcons.python_stroke, AllIcons.todo_20x20_dark, AllIcons.pythonPackages_dark,
+                   AllIcons.services_dark, AllIcons.console_dark]:
             self._vertical.appendChild(SvgIcon.from_file(svg).element)
