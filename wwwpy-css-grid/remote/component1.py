46f155a43b9d9a75e5501d3e891533d9aa17ac06
simone <simone.giacomelli@gmail.com>
2025-07-14 11:45:39 +0100

    Add log functionality and UI updates to Component1
    Introduced a textarea

--- wwwpy-css-grid/remote/component1.py
+++ wwwpy-css-grid/remote/component1.py
@@ -1,3 +1,4 @@
+import inspect
 import wwwpy.remote.component as wpc
 import js
 
@@ -7,6 +8,8 @@ logger = logging.getLogger(__name__)
 
 
 class Component1(wpc.Component, tag_name='component-1'):
+    textarea1: js.HTMLTextAreaElement = wpc.element()
+    _container: js.HTMLDivElement = wpc.element()
 
     def init_component(self):
         # language=html
@@ -28,7 +31,7 @@ body {
 }
 
 </style>    
-<div class="container">
+<div class="container"  data-name="_container">
   <div>One</div>
   <div>Two</div>
   <div>Three</div>
@@ -37,5 +40,20 @@ body {
   <div>Six</div>
   <div>Seven</div>
 </div>
+<textarea data-name="textarea1" placeholder="textarea1" rows="6" wrap="off" 
+style="width: 100%; box-sizing: border-box; margin-top: 1em"></textarea>
 
 """
+
+        cs = js.window.getComputedStyle(self._container)
+        self.log_clear()
+        self.log(f'grid-template-columns: {cs.gridTemplateColumns}')
+        self.log(f'grid-template-rows: {cs.gridTemplateRows}')
+        # cs.gridTemplateRows
+
+    def log_clear(self):
+        self.textarea1.innerHTML = ''
+
+    def log(self, msg):
+        self.textarea1.innerHTML += f'{msg}\n'
+        self.textarea1.scrollTop = self.textarea1.scrollHeight
